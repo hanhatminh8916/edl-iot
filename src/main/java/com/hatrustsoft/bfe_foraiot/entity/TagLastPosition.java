cf4674e56744d9b8bcf370b086cbed4209f256cc
hanhatminh8916 <minhhn.23ns@vku.udn.vn>
2025-11-30 14:45:36 +0700

    Add frontend offline detection - tag turns grey after 30s no data

--- src/main/java/com/hatrustsoft/bfe_foraiot/entity/TagLastPosition.java
+++ src/main/java/com/hatrustsoft/bfe_foraiot/entity/TagLastPosition.java
@@ -10,10 +10,10 @@ import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
-import lombok.Data;
-import lombok.NoArgsConstructor;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 /**
  * Entity lưu vị trí cuối cùng của tag/helmet
