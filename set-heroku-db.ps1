6b7fe45d4089668ab43487688d74896de657ae88
hanhatminh8916 <minhhn.23ns@vku.udn.vn>
2025-12-07 00:10:53 +0700

    Fix database connection: use SPRING_DATASOURCE_URL

--- set-heroku-db.ps1
+++
@@ -1,5 +0,0 @@
-$url = "jdbc:mysql://100.96.40.30:3306/hatrustsoft?useSSL=false&serverTimezone=Asia/Ho_Chi_Minh&allowPublicKeyRetrieval=true&autoReconnect=true"
-$username = "remote_user"
-$password = "MatKhauManh123! "
-
-heroku config:set "SPRING_DATASOURCE_URL=$url" "SPRING_DATASOURCE_USERNAME=$username" "SPRING_DATASOURCE_PASSWORD=$password" --app edl-safework-iot
